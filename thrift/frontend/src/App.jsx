f8247f1d5c069e8a4a8bdf4a36afdbca5fd589ef
Leanne Allen <leanne.allenm@gmail.com>
2024-04-20 22:47:21 -0700

    Added separate folder for adding individual category pages

--- thrift/frontend/src/App.jsx
+++ thrift/frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
+
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { Routes, Route } from "react-router-dom"
 import HomePage from './pages/Home.jsx'
 import SignUpPage from './pages/SignUp.jsx'
@@ -8,27 +10,36 @@ import AboutPage from './pages/About.jsx'
 import SupportPage from './pages/Support.jsx' 
 import ListingPage from './pages/Listings.jsx'
 import NoPage from './pages/NoPage.jsx'
+
 import './App.css'
+import ApparelPage from './pages/categories/Apparel.jsx'
 
 /*ROUTING PATH TEMPLATE: <Route path="" element={</>} /> */
-
 function App() {
   return (
-    <>
-        
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/Home" element={<HomePage />} />
-          <Route path="/Listings" element={<ListingPage/>} />
-          <Route path="/SignUp" element={<SignUpPage />} />
-          <Route path="/Login" element={<LoginPage />} />
-          <Route path="/About" element={<AboutPage />} />
-          <Route path="/Support" element={<SupportPage/>} />  
-          <Route path="*" element={<NoPage />} /> 
-        </Routes>
- 
-    </>
-  )
+    <div>
+      <ApparelPage />
+    </div>
+  );
 }
+
+// FOR ROUTING
+// function App() {
+//   return (
+//     <>
+//         /* <Routes>
+//           <Route path="/" element={<HomePage />} />
+//           <Route path="/Home" element={<HomePage />} />
+//           <Route path="/Listings" element={<ListingPage/>} />
+//           <Route path="/SignUp" element={<SignUpPage />} />
+//           <Route path="/Login" element={<LoginPage />} />
+//           <Route path="/About" element={<AboutPage />} />
+//           <Route path="/Support" element={<SupportPage/>} />  
+//           <Route path="/Apparel" element={<ApparelPage />} />
+//           <Route path="*" element={<NoPage />} /> 
+//         </Routes> */}
+//         </>
+//   );
+// }
 
 export default App
---
+++ thrift/frontend/src/pages/categories/Apparel.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Header from '../../components/Header.jsx'
+
+function ApparelPage() {
+    return (
+        <>
+            <Header/>
+            <h1>Apparel</h1>
+        </>
+    );  
+    }
+export default ApparelPage;
