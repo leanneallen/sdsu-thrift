466e23917d454e4d3b67e0f0e05cd8323c183888
STEVEN BUSSE <sbusse6231@sdsu.edu>
2024-04-21 20:53:28 -0700

    added users/authentification
    needs thorough testing

--- README.md
+++
@@ -1,2 +0,0 @@
-# sdsuthrift
-Source code for our SDSU Thrift website - a website that allows SDSU to buy, sell, and exchange goods
--- thrift/backend/authentification/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def users(request):
-    return HttpResponse("Hello, world. You're at the authentification index.")
