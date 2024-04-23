d7895222ca03f84a4ff055db1e9ea9e3b85c754a
Leanne Allen <leanne.allenm@gmail.com>
2024-04-22 19:48:23 -0700

    Made carousel buttons better placed, image background to accomodate potential smaller image uploads

--- thrift/frontend/src/components/ResponsiveCarousel.jsx
+++ thrift/frontend/src/components/ResponsiveCarousel.jsx
@@ -30,6 +30,7 @@ const ResponsiveCarousel = forwardRef((props, ref) => {
       height: { xs: '50vh', sm: '60vh', md: '70vh' }, // responsive height based on breakpoints
       
     }}>
+
       {/* Blurry background container */}
       <Box className="blurry-background-container" sx={{
         backgroundImage: `url(${listingArr[currentIndex]})`,
@@ -42,10 +43,10 @@ const ResponsiveCarousel = forwardRef((props, ref) => {
         height: '100%',
       }}>
       </Box>
+
       {/* Image buttons container */}
-      <Box className='image-buttons-container' sx={{ position: 'absolute',alignItems: 'center',
-      justifyContent: 'center',
-      display: 'flex', top: 0, left: 0, width: '100%', height: '100%' }}>
+      <Box className='image-buttons-container' sx={{ position: 'absolute',alignItems: 'center', justifyContent: 'center', display: 'flex', top: 0, left: 0, width: '100%', height: '100%' }}>
+        
         <ArrowCircleLeftRoundedIcon onClick={goToPrevSlide} sx={{ position: 'absolute', left: 16, zIndex: 2, cursor: 'pointer' }} />
         <Box component='img' src={listingArr[currentIndex]} alt={`Image ${currentIndex + 1}`} sx={{
           maxWidth: '100%',
@@ -55,70 +56,14 @@ const ResponsiveCarousel = forwardRef((props, ref) => {
           objectFit: 'contain',
           zIndex: 1
         }} />
+        
         <ArrowCircleRightRoundedIcon onClick={goToNextSlide} sx={{ position: 'absolute', right: 16, zIndex: 2, cursor: 'pointer' }} />
+      
       </Box>
+
     </Box>
   );
 });
 
 export default ResponsiveCarousel;
 
-
-
-
-// import React, { useState, forwardRef } from 'react';
-// import { Box } from '@mui/material';
-// import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRounded';
-// import ArrowCircleLeftRoundedIcon from '@mui/icons-material/ArrowCircleLeftRounded';
-
-// const listingArr = [
-//   "./src/assets/frog1.jpg",
-//   "./src/assets/frog2.png",
-//   "./src/assets/frog3.jpg",
-//   "./src/assets/frog4.jpg",
-//   "./src/assets/frog5.jpg",
-//   "./src/assets/frog6.jpg"
-// ];
-
-// const ResponsiveCarousel = forwardRef((props, ref) => {
-//   const [currentIndex, setCurrentIndex] = useState(0);
-
-//   const goToNextSlide = () => {
-//     setCurrentIndex(currentIndex === listingArr.length - 1 ? 0 : currentIndex + 1);
-//   };
-
-//   const goToPrevSlide = () => {
-//     setCurrentIndex(currentIndex === 0 ? listingArr.length - 1 : currentIndex - 1);
-//   };
-
-//   return (
-//     <Box ref={ref} className="carousel-container" sx={{
-//       backgroundImage: `url(${listingArr[currentIndex]})`, // initial image
-//       backgroundRepeat: 'no-repeat',
-//       backgroundPosition: 'center',
-//       backgroundSize: 'cover',
-//       filter: 'blur(5px)',
-//       zIndex: 0,
-//       width: '100%', // full width of the container
-//       height: { xs: '50vh', sm: '60vh', md: '70vh' }, // responsive height based on breakpoints
-//       display: 'flex',
-//       alignItems: 'center',
-//       justifyContent: 'center',
-//       position: 'relative'
-//     }}>
-//       <ArrowCircleLeftRoundedIcon onClick={goToPrevSlide} sx={{ position: 'absolute', left: 0, zIndex: 2, cursor: 'pointer' }} />
-//       <Box component='img' src={listingArr[currentIndex]} alt={`Image ${currentIndex + 1}`} sx={{
-//         maxWidth: '100%', // ensures image does not exceed the container's width
-//         maxHeight: '100%', // ensures image does not exceed the container's height
-//         width: 'auto', // allows the image to scale responsively
-//         height: 'auto', // allows the image to scale responsively
-//         objectFit: 'contain', // maintains aspect ratio without cropping
-//         zIndex: 1
-//       }} />
-//       <ArrowCircleRightRoundedIcon onClick={goToNextSlide} sx={{ position: 'absolute', right: 0, zIndex: 2, cursor: 'pointer' }} />
-    
-//     </Box>
-//   );
-// });
-
-// export default ResponsiveCarousel;
