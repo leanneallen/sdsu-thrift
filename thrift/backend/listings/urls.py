05859805ae0ba162041df0640dd7fbb976db7186
jambho <jamal.bhola@gmail.com>
2024-04-23 19:59:21 -0700

    Main cleanup post revert

--- thrift/backend/listings/urls.py
+++ thrift/backend/listings/urls.py
@@ -1,16 +1,11 @@
 from django.urls import path, include
 from django.contrib import admin
 from rest_framework import routers
-from listings.views import ListingsViewSet, CategoryViewSet
+from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
-router = routers.DefaultRouter()
-router.register(r'listings', ListingsViewSet, basename='listings')
-router.register(r'category', CategoryViewSet, basename='category')
-
 urlpatterns = [
-
      #path('', include(router.urls)),
      path('listings/<str:category>/', views.getListings, name="listings"),
      ]
