58326fd6fff674d9528c93c60b2a39c87f1e3492
STEVEN BUSSE <sbusse6231@sdsu.edu>
2024-04-23 22:35:42 -0700

    footer
    five dolla five dolla five dolla footlong

--- thrift/frontend/src/pages/Footer.jsx
+++ thrift/frontend/src/pages/Footer.jsx
@@ -1,14 +1,21 @@
-// Footer.jsx
 import React from 'react';
-import { Box, TextField, Button, IconButton, Typography } from '@mui/material';
+import { Box, TextField, Button, IconButton, Typography, useTheme } from '@mui/material';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Footer = () => {
+    const theme = useTheme(); // Use theme for styling
+
     return (
-        <Box sx={{ width: '100%', padding: 3, backgroundColor: '#f0f0f0', textAlign: 'center' }}>
-            <Typography variant="h6" gutterBottom>
+        <Box sx={{
+            width: '100%',
+            padding: 3,
+            textAlign: 'center',
+            backgroundColor: theme.palette.background.paper, // Adaptive background color
+            color: theme.palette.text.primary, // Adaptive text color
+        }}>
+            <Typography variant="h6" gutterBottom sx={{ color: 'text.primary' }}>
                 Subscribe to Our Newsletter
             </Typography>
             <Box
