94b396553caeea9b16cb7b8737aa0868467db93f
Leanne Allen <leanne.allenm@gmail.com>
2024-04-30 18:33:10 -0700

    Routing name for Apparel fixed

--- thrift/frontend/src/components/Category.jsx
+++ thrift/frontend/src/components/Category.jsx
@@ -8,7 +8,7 @@ import { Link as RouterLink } from "react-router-dom";
 export default function CategoryCard({categoryName, onClick}) {
   return (
     <Card sx={{ alignItems: 'center', justifyContent: 'center', display: 'flex', backgroundColor: '#A6192E', height: '150px' }}>
-      <RouterLink to="/apparel" style={{ textDecoration: 'none', color: 'inherit', width: '100%', height: '100%' }}>
+      <RouterLink to="/Apparel" style={{ textDecoration: 'none', color: 'inherit', width: '100%', height: '100%' }}>
       <CardActionArea onClick={onClick} sx={{ width: '100%', height: '100%' }}>
         <CardContent>
           <Typography sx={{ color: 'white' }} component="div">
